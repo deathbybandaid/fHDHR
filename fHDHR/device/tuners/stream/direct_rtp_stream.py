86ab4d45422d5153a56731623257e956899e103b
deathbybandaid <github@deathbybandaid.net>
2021-02-02 11:48:27 -0500

    test

--- fHDHR/device/tuners/stream/direct_rtp_stream.py
+++ fHDHR/device/tuners/stream/direct_rtp_stream.py
@@ -51,10 +51,7 @@ class Direct_RTP_Stream():
         if self.stream_args["transcode_quality"]:
             self.fhdhr.logger.info("Client requested a %s transcode for stream. Direct Method cannot transcode." % self.stream_args["transcode_quality"])
 
-        if self.stream_args["stream_info"]["headers"]:
-            req = self.fhdhr.web.session.get(self.stream_args["stream_info"]["url"], stream=True, headers=self.stream_args["stream_info"]["headers"])
-        else:
-            req = self.fhdhr.web.session.get(self.stream_args["stream_info"]["url"], stream=True)
+        self.socket.send(str.encode(self.stream_args["stream_info"]["url"]))
 
         def generate():
 
@@ -64,24 +61,23 @@ class Direct_RTP_Stream():
 
                 while self.tuner.tuner_lock.locked():
 
-                    for chunk in req.iter_content(chunk_size=self.bytes_per_read):
+                    chunk = self.socket.recv(self.bytes_per_read)
 
-                        if (not self.stream_args["duration"] == 0 and
-                           not time.time() < self.stream_args["time_end"]):
-                            req.close()
-                            self.fhdhr.logger.info("Requested Duration Expired.")
-                            self.tuner.close()
+                    if (not self.stream_args["duration"] == 0 and
+                       not time.time() < self.stream_args["time_end"]):
+                        self.fhdhr.logger.info("Requested Duration Expired.")
+                        self.tuner.close()
 
-                        if not chunk:
-                            break
-                            # raise TunerError("807 - No Video Data")
+                    if not chunk or not len(chunk):
+                        break
+                        # raise TunerError("807 - No Video Data")
 
-                        chunk_size = int(sys.getsizeof(chunk))
-                        self.fhdhr.logger.info("Passing Through Chunk #%s with size %s" % (chunk_counter, chunk_size))
-                        yield chunk
-                        self.tuner.add_downloaded_size(chunk_size)
+                    chunk_size = int(sys.getsizeof(chunk))
+                    self.fhdhr.logger.info("Passing Through Chunk #%s with size %s" % (chunk_counter, chunk_size))
+                    yield chunk
+                    self.tuner.add_downloaded_size(chunk_size)
 
-                        chunk_counter += 1
+                    chunk_counter += 1
 
                 self.fhdhr.logger.info("Connection Closed: Tuner Lock Removed")
 
@@ -90,7 +86,6 @@ class Direct_RTP_Stream():
             except Exception as e:
                 self.fhdhr.logger.info("Connection Closed: %s" % e)
             finally:
-                req.close()
                 self.fhdhr.logger.info("Connection Closed: Tuner Lock Removed")
                 if hasattr(self.fhdhr.origins.origins_dict[self.tuner.origin], "close_stream"):
                     self.fhdhr.origins.origins_dict[self.tuner.origin].close_stream(self.tuner.number, self.stream_args)
