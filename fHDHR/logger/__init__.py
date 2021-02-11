ea101e15d61b0dbbfba9caed3e0741bb142694d4
deathbybandaid <github@deathbybandaid.net>
2021-02-11 08:17:33 -0500

    test

--- fHDHR/logger/__init__.py
+++ fHDHR/logger/__init__.py
@@ -71,14 +71,15 @@ class Logger():
         f_handler = logging.FileHandler(log_file)
         # c_handler.setLevel(log_level)
         f_handler.setLevel(log_level)
-        formatter = ColoredFormatter()
-        f_handler.setFormatter(formatter)
 
         # Create formatters and add it to handlers
         # c_format = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
-        f_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        # f_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
         # c_handler.setFormatter(c_format)
-        f_handler.setFormatter(f_format)
+        # f_handler.setFormatter(f_format)
+
+        formatter = ColoredFormatter()
+        f_handler.setFormatter(formatter)
 
         # Add handlers to the logger
         # logger.addHandler(c_handler)
