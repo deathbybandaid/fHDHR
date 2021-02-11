328f3fcb8742181871bf19e436f3f331f5e9f10b
deathbybandaid <github@deathbybandaid.net>
2021-02-11 16:07:04 -0500

    test

--- fHDHR/device/tuners/stream/direct_m3u8_stream.py
+++ fHDHR/device/tuners/stream/direct_m3u8_stream.py
@@ -1,7 +1,7 @@
 import sys
 import time
 import m3u8
-
+from collections import OrderedDict
 from Crypto.Cipher import AES
 
 # from fHDHR.exceptions import TunerError
@@ -26,14 +26,20 @@ class Direct_M3U8_Stream():
         if self.stream_args["transcode_quality"]:
             self.fhdhr.logger.info("Client requested a %s transcode for stream. Direct Method cannot transcode." % self.stream_args["transcode_quality"])
 
+        segments_dict = OrderedDict()
+        start_time = time.time()
+        total_secs_served = 0
+
         def generate():
+            total_chunks = 0
 
             try:
 
-                played_chunk_urls = []
-
                 while self.tuner.tuner_lock.locked():
 
+                    added, removed = 0, 0
+
+                    # (re)Load the m3u8 playlist, apply headers if needbe
                     try:
                         if self.stream_args["stream_info"]["headers"]:
                             playlist = m3u8.load(self.stream_args["stream_info"]["url"], headers=self.stream_args["stream_info"]["headers"])
@@ -46,52 +52,72 @@ class Direct_M3U8_Stream():
 
                     segments = playlist.segments
 
-                    if len(played_chunk_urls):
-                        newsegments = 0
-                        for segment in segments:
-                            if segment.absolute_uri not in played_chunk_urls:
-                                newsegments += 1
-                        self.fhdhr.logger.info("Refreshing m3u8, Loaded %s new segments." % str(newsegments))
-                    else:
-                        self.fhdhr.logger.info("Loaded %s segments." % str(len(segments)))
-
                     if playlist.keys != [None]:
-                        keys = [{"url": key.absolute_uri, "method": key.method, "iv": key.iv} for key in playlist.keys if key]
+                        keys = [{"uri": key.absolute_uri, "method": key.method, "iv": key.iv} for key in playlist.keys if key]
                     else:
                         keys = [None for i in range(0, len(segments))]
 
+                    # Only add new segments to our segments dict
                     for segment, key in zip(segments, keys):
-                        chunkurl = segment.absolute_uri
+                        uri = segment.absolute_uri
+                        if uri not in segments_dict:
+                            segments_dict[uri] = {
+                                                  "played": False,
+                                                  "duration": segment.duration,
+                                                  "key": key
+                                                  }
+                            added += 1
+                            self.fhdhr.logger.debug("Adding %s to play queue." % uri)
 
-                        if chunkurl and chunkurl not in played_chunk_urls:
-                            played_chunk_urls.append(chunkurl)
+                            segments[uri]["last_seen"] = time.time()
 
+                    # Cleanup Play Queue
+                    for uri, data in list(segments_dict.items()):
+                        if data["played"] and (time.time() - data["last_seen"]) > 10:
+                            self.fhdhr.logger.debug("Removed %s from play queue." % uri)
+                            del segments[uri]
+                            removed += 1
+
+                    self.fhdhr.logger.info("Refreshing m3u8, Loaded %s new segments, removed %s" % (added, removed))
+
+                    for uri, data in list(segments_dict.items()):
+                        if not data["played"]:
+
+                            total_chunks += 1
+
+                            self.fhdhr.logger.debug("Downloading Chunk #%s: %s" % (total_chunks, uri))
+                            if self.stream_args["stream_info"]["headers"]:
+                                chunk = self.fhdhr.web.session.get(uri, headers=self.stream_args["stream_info"]["headers"]).content
+                            else:
+                                chunk = self.fhdhr.web.session.get(uri).content
+
+                            if data["key"]:
+                                if data["key"]["uri"]:
+                                    if self.stream_args["stream_info"]["headers"]:
+                                        keyfile = self.fhdhr.web.session.get(data["key"]["uri"], headers=self.stream_args["stream_info"]["headers"]).content
+                                    else:
+                                        keyfile = self.fhdhr.web.session.get(data["key"]["uri"]).content
+                                    cryptor = AES.new(keyfile, AES.MODE_CBC, keyfile)
+                                    self.fhdhr.logger.debug("Decrypting Chunk #%s with key: %s" % (total_chunks, data["key"]["uri"]))
+                                    chunk = cryptor.decrypt(chunk)
+
+                            data['played'] = True
+
+                            if not chunk:
+                                break
+                                # raise TunerError("807 - No Video Data")
+
+                            chunk_size = int(sys.getsizeof(chunk))
+                            self.fhdhr.logger.info("Passing Through Chunk #%s with size %s" % (total_chunks, chunk_size))
+                            yield chunk
+                            self.tuner.add_downloaded_size(chunk_size)
+
+                            """
                             if (not self.stream_args["duration"] == 0 and
                                not time.time() < self.stream_args["time_end"]):
                                 self.fhdhr.logger.info("Requested Duration Expired.")
                                 self.tuner.close()
-
-                            if self.stream_args["stream_info"]["headers"]:
-                                chunk = self.fhdhr.web.session.get(chunkurl, headers=self.stream_args["stream_info"]["headers"]).content
-                            else:
-                                chunk = self.fhdhr.web.session.get(chunkurl).content
-                            if not chunk:
-                                break
-                                # raise TunerError("807 - No Video Data")
-                            if key:
-                                if key["url"]:
-                                    if self.stream_args["stream_info"]["headers"]:
-                                        keyfile = self.fhdhr.web.session.get(key["url"], headers=self.stream_args["stream_info"]["headers"]).content
-                                    else:
-                                        keyfile = self.fhdhr.web.session.get(key["url"]).content
-                                    cryptor = AES.new(keyfile, AES.MODE_CBC, keyfile)
-                                    self.fhdhr.logger.info("Decrypting Chunk #%s with key: %s" % (len(played_chunk_urls), key["url"]))
-                                    chunk = cryptor.decrypt(chunk)
-
-                            chunk_size = int(sys.getsizeof(chunk))
-                            self.fhdhr.logger.info("Passing Through Chunk #%s with size %s: %s" % (len(played_chunk_urls), chunk_size, chunkurl))
-                            yield chunk
-                            self.tuner.add_downloaded_size(chunk_size)
+                            """
 
                 self.fhdhr.logger.info("Connection Closed: Tuner Lock Removed")
 
