e7e52e8f0cd002f3b66b3412741b9d7227eb793c
deathbybandaid <github@deathbybandaid.net>
2021-02-17 09:44:08 -0500

    test

--- fHDHR_web/pages/versions_html.py
+++ fHDHR_web/pages/versions_html.py
@@ -1,7 +1,7 @@
 from flask import request, render_template, session
 
 
-class Version_HTML():
+class Versions_HTML():
     endpoints = ["/versions", "/versions.html"]
     endpoint_name = "page_versions_html"
     endpoint_access_level = 1
