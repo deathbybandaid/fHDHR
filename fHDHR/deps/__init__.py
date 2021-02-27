ac2abef047006e1eb9583c3afc1856621f2615ea
Sam Zick <github@deathbybandaid.net>
2021-02-27 10:41:13 -0500

    test

--- fHDHR/deps/__init__.py
+++ fHDHR/deps/__init__.py
@@ -1,6 +1,9 @@
+import pathlib
 
 
 class Dependencies():
 
     def __init__(self, script_dir):
-        print("here at " + str(script_dir))
+        self.script_dir = script_dir
+        self.core_req = pathlib.Path(script_dir).joinpath('requirements.txt')
+        print(self.core_req)
