de84ef3dc08ac45759d6c921e2e0ae71a580447a
Sam Zick <github@deathbybandaid.net>
2021-02-27 10:43:15 -0500

    test

--- fHDHR/deps/__init__.py
+++ fHDHR/deps/__init__.py
@@ -1,7 +1,7 @@
 import pathlib
 
 from pip._internal import main as pipmain
-from pip._internal.utils.misc import get_installed_distributions as getpiplist
+from pip._internal.utils.misc import get_installed_distributions
 
 
 class Dependencies():
@@ -9,4 +9,6 @@ class Dependencies():
     def __init__(self, script_dir):
         self.script_dir = script_dir
         self.core_req = pathlib.Path(script_dir).joinpath('requirements.txt')
-        print(self.core_req)
+
+        self.pipinstalled = sorted(["%s" % (i.key) for i in get_installed_distributions()])
+        print(self.pipinstalled)
