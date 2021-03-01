55abb6f3b570a930584e9b9b9c4354b1b7f1b91b
Sam Zick <github@deathbybandaid.net>
2021-03-01 09:14:43 -0500

    test

--- main.py
+++ main.py
@@ -2,18 +2,16 @@
 # coding=utf-8
 # pylama:ignore=E402
 
+from gevent import monkey
+monkey.patch_all(ssl=False)
+
 import os
 import sys
 import pathlib
 SCRIPT_DIR = pathlib.Path(os.path.dirname(os.path.abspath(__file__)))
 
-print("a")
 from deps import Dependencies
 deps = Dependencies(SCRIPT_DIR)
-print("b")
-
-from gevent import monkey
-monkey.patch_all(ssl=False)
 
 from fHDHR.cli import run
 import fHDHR_web
