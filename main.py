2830da186ceb9bd9a6aafa9e5607ef6ac430e414
Sam Zick <github@deathbybandaid.net>
2021-03-01 08:57:16 -0500

    test

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 # coding=utf-8
 # pylama:ignore=E402
+from gevent import monkey
+monkey.patch_all()
+
 import os
 import sys
 import pathlib
@@ -13,6 +16,4 @@ from fHDHR.cli import run
 import fHDHR_web
 
 if __name__ == "__main__":
-    from gevent import monkey
-    monkey.patch_all(ssl=False)
     sys.exit(run.main(SCRIPT_DIR, fHDHR_web, deps))
