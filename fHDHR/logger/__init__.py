74fbe932540cbb056eb124693ec8cb9e95a51c65
deathbybandaid <github@deathbybandaid.net>
2021-03-08 09:48:37 -0500

    test

--- fHDHR/logger/__init__.py
+++ fHDHR/logger/__init__.py
@@ -9,8 +9,17 @@ class MemLogger(logging.StreamHandler):
     dict = OrderedDict()
     level = 0
 
+    """
+    ['__class__', '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__gt__', '__hash__', '__init__', '__init_subclass__', '__le__', '__lt__', '__module__', '__ne__', '__new__', '__reduce__',
+    '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__',
+
+    'args', 'asctime', 'created', 'exc_info', 'exc_text', 'filename', 'funcName', 'getMessage', 'levelname', 'levelno', 'lineno',
+    'message', 'module', 'msecs', 'msg', 'name', 'pathname', 'process', 'processName', 'relativeCreated',
+    'stack_info', 'thread', 'threadName']
+    """
+
     def emit(self, record):
-        print(dir(record))
+        print(record.levelno)
 
 
 class Logger():
