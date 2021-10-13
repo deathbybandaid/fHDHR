206ab646680f39f02a989c575f8d50e5f6173992
deathbybandaid <github@deathbybandaid.net>
2021-10-13 11:07:18 -0400

    test

--- fHDHR_web/api/scheduler.py
+++ fHDHR_web/api/scheduler.py
@@ -29,7 +29,7 @@ class Scheduler_API():
             for job_dict in jobsdicts:
                 for run_item in ["last_run", "next_run"]:
                     if job_dict[run_item]:
-                        job_dict[run_item] =job_dict[run_item].total_seconds()
+                        job_dict[run_item] = job_dict[run_item].timestamp()
                 formatted_jobsdicts.append(job_dict)
 
             return_json = json.dumps(formatted_jobsdicts, indent=4)
--- fHDHR_web/pages/scheduler_html.py
+++ fHDHR_web/pages/scheduler_html.py
@@ -24,7 +24,7 @@ class Scheduler_HTML():
         for job_dict in jobsdicts:
             for run_item in ["last_run", "next_run"]:
                 if job_dict[run_item]:
-                    ts = job_dict[run_item].total_seconds()
+                    ts = job_dict[run_item].timestamp()
                     if job_dict[run_item] > nowtime:
                         job_dict[run_item] = humanized_time(ts - nowtime)
                     else:
