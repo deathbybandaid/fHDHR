701a9b88d4f04b88fc6d817e6f43328991880a97
deathbybandaid <github@deathbybandaid.net>
2022-02-19 12:59:09 -0500

    test

--- fHDHR/origins/__init__.py
+++ fHDHR/origins/__init__.py
@@ -94,4 +94,5 @@ class Origins():
                     # Set Origin attributes if missing
                     if not hasattr(self.origins_dict[method], default_setting):
                         self.fhdhr.logger.debug("Setting %s %s attribute to: %s" % (method, default_setting, self.fhdhr.config.dict[method][default_setting]))
-                        setattr(self.origins_dict[method], default_setting, self.fhdhr.config.dict[method][default_setting])
+                        exec("%s.%s = property(lambda self: %s)" % (self.origins_dict[method], default_setting, self.fhdhr.config.dict[method][default_setting]))
+                        print(eval("%s.%s" % (self.origins_dict[method], default_setting)))
