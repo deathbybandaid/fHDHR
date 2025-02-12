8cfc974515c19971b9a437cb1b661f6a27df6d31
deathbybandaid <github@deathbybandaid.net>
2025-02-12 13:31:37 -0500

    test

--- fHDHR/device/epg/epg_handler.py
+++ fHDHR/device/epg/epg_handler.py
@@ -22,7 +22,7 @@ class EPG_Handler():
         self.origins = origins
         self.channels = origins.channels
 
-        print(self.channels.get_channel_list())
+        print(self.channels.get_channel_list("nextpvr"))
 
         self._epgdict = {}
 
