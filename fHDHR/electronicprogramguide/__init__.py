c56e3f9ad21d48deb70e06f550d62c3295e731c5
deathbybandaid <github@deathbybandaid.net>
2025-02-12 13:01:11 -0500

    test

--- fHDHR/electronicprogramguide/__init__.py
+++ fHDHR/electronicprogramguide/__init__.py
@@ -14,6 +14,43 @@ class EPG_Handlers():
 
         self.selfadd_epg_handlers()
 
+        print(list_epg_handlers)
+
+    @property
+    def list_epg_handlers(self):
+        return [epg_handler_name for epg_handler_name in list(self.epg_handlers_dict.keys())]
+
+    @property
+    def count_epg_handlers(self):
+        return len(self.list_epg_handlers)
+
+    @property
+    def first_epg_handler(self):
+        if self.count_epg_handlers:
+            return self.list_epg_handlers[0]
+        return None
+
+    def get_epg_handler_obj(self, epg_handler_name):
+        if epg_handler_name not in self.list_epg_handlers:
+            return None
+        return self.epg_handlers_dict[epg_handler_name]
+
+    def get_epg_handler_conf(self, epg_handler_name):
+        conf_dict = {}
+        if epg_handler_name not in self.list_epg_handlers:
+            return conf_dict
+        return self.epg_handlers_dict[epg_handler_name].get_epg_handler_conf()
+
+    def get_epg_handler_property(self, epg_handler_name, epg_handler_attr):
+        if epg_handler_name not in self.list_epg_handlers:
+            return None
+        return self.epg_handlers_dict[epg_handler_name].get_epg_handler_property(epg_handler_attr)
+
+    def epg_handler_has_method(self, epg_handler_name, epg_handler_attr):
+        if epg_handler_name not in self.list_epg_handlers:
+            return None
+        return self.epg_handlers_dict[epg_handler_name].has_method(epg_handler_attr)
+
     def selfadd_epg_handlers(self):
         """
         Import EPG_Handlers.
